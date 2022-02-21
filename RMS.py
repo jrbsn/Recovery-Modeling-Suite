ff1a53498836f708899651b33d1f60d5eb3b8697
jrbsn <98776682+jrbsn@users.noreply.github.com>
2022-02-21 16:46:35 -0600

    program outline
    outline not even close to done

---
+++ RMS.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import tkinter as tk
+from tkinter import ttk
+import matplotlib as plt
+
+# general_inputs: depl. alt, landing alt, vls, horz/vertical
+# shape_params: cd, avt, cfc
+# prel_calcs: cds, tf
+
+
+class Inflation: # all things specifically involving inflation
+    def __init__(self):
+        pass
+    def inflation_time(self, inputs):
+        pass
+    def inflation_curve_method(self, general_inputs, diameter, shape_params, prel_calcs):
+        pass
+
+class Interface(tk.Tk): # graphical user interface using tkinter
+    def __init__(self):
+        super.__init__()
+
+def main():
+    pass
